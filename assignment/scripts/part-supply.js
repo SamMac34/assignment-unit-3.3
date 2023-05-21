1d2f899ef0f30230b3f160ff38b301bb5c081de7
Sam McCauley <sammccauley34@gmail.com>
2023-05-21 00:13:39 -0500

    Completed question 9

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -75,11 +75,11 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
-//let parts = 572;
-//let box = 0;
-//while( parts < 572 ) {
-
-    //parts++;
-//}
-//console.log('Boxes filled: ');
-//console.log('Parts leftover: ');
+let parts = 572;
+let box = 0;
+while( parts >= 7 ) {
+    parts = parts - 7;
+    box++;
+}
+console.log('Boxes filled: ' + box);
+console.log('Parts leftover: ' + parts);
