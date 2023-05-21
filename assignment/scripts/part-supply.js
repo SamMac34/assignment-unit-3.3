d4dfffcd77f055785013173b3a5b3b1264b0dd2d
Sam McCauley <sammccauley34@gmail.com>
2023-05-20 23:18:44 -0500

    Completed questions 7 and 8, added question to index.html

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -49,10 +49,24 @@ for( let x=0; x<supplyChanges.length; x++ ) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for( x in supplyChanges ) {
+    console.log(supplyChanges[x]);
+    if( supplyChanges[x] > 0 ) {
+        console.log('Added', supplyChanges[x], 'parts.');
+    } else if( supplyChanges[x] < 0 ) {
+        console.log('Removed', supplyChanges[x], 'parts.')
+    } else{
+        console.log('No Change.')
+    }
+    }
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let sum = 0;
+for( x in supplyChanges ) {
+    sum += supplyChanges[x];
+}
+console.log(sum);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -61,3 +75,6 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+const parts = 572;
+let box = 0;
+//while( parts < ) 
